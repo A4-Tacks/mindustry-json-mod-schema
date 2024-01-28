879190551e08cffa35f0c806eeee623598ebe4d0
A4-Tacks <wdsjxhno1001@163.com>
2024-01-28 11:07:21 +0800

    添加物品的schema

---
+++ fmt.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+set -o nounset
+set -o errtrace
+function catch_error {
+    local LEC=$? name i line file
+    echo "Traceback (most recent call last):" >&2
+    for ((i = ${#FUNCNAME[@]} - 1; i >= 0; --i)); do
+        name="${FUNCNAME[$i]}"
+        line="${BASH_LINENO[$i]}"
+        file="${BASH_SOURCE[$i]}"
+        echo "  File ${file@Q}, line ${line}, in ${name@Q}" >&2
+    done
+    echo "Error: [ExitCode: ${LEC}]" >&2
+    exit "${LEC}"
+}
+trap catch_error ERR
+
+temp=$(mktemp)
+trap 'rm "$temp"' EXIT
+
+find ./* -name '*.json' -print0 | while read -rd '' file; do
+    jq . --tab < "${file}" | sed 's/\t/    /g' > "${temp}"
+    cat "${temp}" > "${file}"
+done
